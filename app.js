a5a051bf9f808266c9ed8f24c3656a0b5c1fbaf8
Aditya <151871674+aditya15nautiyal@users.noreply.github.com>
2025-01-08 23:10:32 +0530

    adding listing model and integrating it
    create a new listing model and integrated it to the app.js

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const Listing = require('./models/listing');
 
 const port = 8080;
 
+main()
+    .then(() => {
+        console.log("connected to database");
+    }).catch((err) => {
+        console.log(err);
+    });
+
+async function main() {
+    await mongoose.connect("mongodb://localhost:27017/wanderlust");
+}
 
 app.get("/", (req, res) => {
     res.send("connected to server!");
---
+++ models/listing.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const listingSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String
+    },
+    image: {
+        type: String,
+        set: (v) => (v === "") ?
+            "https://images.unsplash.com/photo-1736159427273-189b0e49f19b?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,
+        default: "https://images.unsplash.com/photo-1736159427273-189b0e49f19b?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    price: {
+        type: Number
+    },
+    location: {
+        type: String
+    },
+    country: {
+        type: String
+    }
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
