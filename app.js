1b58432c0a488ff50ef0b7a99cd1ebf670aedd93
Aditya <151871674+aditya15nautiyal@users.noreply.github.com>
2025-01-10 22:53:53 +0530

    Added show route, and some minor changes

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const port = 8080;
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
 
 main()
     .then(() => {
@@ -24,11 +25,25 @@ app.get("/", (req, res) => {
     res.send("connected to server!");
 });
 
+// Index Route - show all the possible listings
 app.get("/listings", async (req, res) => {
     const allListings = await Listing.find({});
-    res.render("listings/index.ejs", {allListings});
+    res.render("listings/index.ejs", { allListings });
 });
 
+//Show Route - show details of a particular listing
+app.get("/listings/:id", async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    console.log("Displaying listing with id: " + id);
+    res.render("listings/show.ejs", { listing });
+});
+
+
+app.get("*", (req, res) => {
+    console.log("Wrong URL!");
+    res.send("404! Page not found");
+});
 
 app.listen(port, () => {
     console.log("listening to port " + port);
