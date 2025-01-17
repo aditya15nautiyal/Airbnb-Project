dda3b0235aa5f57f4c01e0af0c69e3c2a2dc96de
Aditya <151871674+aditya15nautiyal@users.noreply.github.com>
2025-01-17 22:31:46 +0530

    creating error page

--- app.js
+++ app.js
@@ -102,7 +102,8 @@ app.all("*", (req, res, next) => {
 
 app.use((err, req, res, next) => {
     let { statusCode = 500, message = "Something went wrong" } = err;
-    res.status(statusCode).send(message);
+    // res.status(statusCode).send(message);
+    res.status(statusCode).render("error.ejs", { err });
 });
 
 
